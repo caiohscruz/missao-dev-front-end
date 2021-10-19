44aff0ac344842f9c4f3afc3ba539bc791bfe680
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-19 09:32:15 -0300

    Setup initial estruture

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import CoursePanel from "../views/CoursePanel.vue";
+import AdministrationPanel from "../views/AdministrationPanel.vue";
+import CourseMaintenance from "../views/CourseMaintenance.vue";
 
 Vue.use(VueRouter);
 
@@ -11,14 +14,25 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/course/:id",
+    name: "CoursePanel",
+    component: CoursePanel
   },
+  {
+    path: "/admin",
+    name: "AdministrationPanel",
+    component: AdministrationPanel
+  },
+  {
+    path: "/admin/course/new",
+    name: "CourseMaintenance",
+    component: CourseMaintenance
+  },
+  {
+    path: "/admin/course/edit/:id",
+    name: "CourseMaintenance",
+    component: CourseMaintenance
+  }
 ];
 
 const router = new VueRouter({
