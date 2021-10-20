b4068e72e7af03d349caf5fff77d905fb1322b20
caiohscruz <caiohscruz@hotmail.com>
2021-10-20 00:11:52 -0300

    add course panel

--- src/views/CoursePanel.vue
+++ src/views/CoursePanel.vue
@@ -1,11 +1,38 @@
 <template>
-  
+    <div>
+        <div>
+            <h1>{{course.title}}</h1>
+            <p>
+                {{course.description}}
+            </p>
+        </div>
+        <div>
+            <img :src="course.cover" />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+    import axios from "axios";
 
-}
+    export default {
+        props: {
+            id: Number,
+        },
+        data(){
+            return{
+                course : Object,
+                lessons: [],
+            }
+        },
+        created() {
+            axios.get('https://dashboard.heroku.com/apps/demo-api-anima-project/course/' + this.id).then(res=>{
+                this.course = res.data;
+            })
+
+
+        }
+    }
 </script>
 
 <style>
