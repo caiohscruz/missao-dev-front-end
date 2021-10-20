d32b87e49352b2b84cfd5edd5e687f18cd4535f3
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-20 10:10:09 -0300

    add lessoncard

--- src/components/LessonCard.vue
+++ src/components/LessonCard.vue
@@ -1,14 +1,31 @@
 <template>
-  <div></div>
-  
+  <div>
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        :src="lesson.link"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div>
+      <h3>{{ lesson.title }}</h3>
+      <p>
+        {{ lesson.description }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    lesson: Object,
+  },
+};
 </script>
 
 <style>
-
 </style>
