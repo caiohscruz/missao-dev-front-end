10288c55ec4dc67316a3ffdc17558f1d5c04b1a0
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-20 11:56:16 -0300

    upgrade estruture form lesson

--- src/components/FormLesson.vue
+++ src/components/FormLesson.vue
@@ -1,13 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <h5>Aula {{index}}</h5>
+    <input type="text" placeholder="Título do curso" v-model="lesson.title">
+    <input type="text" placeholder="Url do vídeo" v-model="lesson.link">
+    <input type="text" placeholder="Descrição do curso" v-model="lesson.description">
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props:{
+    lesson: Object,
+    index: Number
+  }
+};
 </script>
 
 <style>
+input[type="text"] {
+  border: 1px var(--input-border) solid;
+  background-color: var(--input-background);
+  color: var(--input-text);
+  font-size: 1.8rem;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+}
 
+input::placeholder {
+  color: var(--input-text);
+}
 </style>
