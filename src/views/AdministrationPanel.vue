75b37bf174404257907bdf5cb13de2bdde1232b5
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-20 10:29:28 -0300

    update estruture adm panel

--- src/views/AdministrationPanel.vue
+++ src/views/AdministrationPanel.vue
@@ -5,20 +5,22 @@
 
     <table>
       <tr>
-        <td class="table-header">
-          Curso
-        </td>
-        <td class="table-header">
-          Ações
-        </td>
+        <td class="table-header">Curso</td>
+        <td class="table-header actions">Ações</td>
       </tr>
-      <tr v-for="(course, index) in courses" :key="index">
+      <tr v-for="(course, index) in courses" :key="index" class="course">
         <td>
-          {{course.title}}
+          {{ course.title }}
         </td>
-        <td>
-          <button class="warning-btn">editar</button>
-          <button class="danger-btn">excluir</button>
+        <td class="actions">
+          <button class="warning-btn">
+            <router-link :to="'/admin/course/edit/' + course.id">
+              editar
+            </router-link>
+          </button>
+           <button class="danger-btn">
+              excluir
+          </button>
         </td>
       </tr>
     </table>
@@ -26,48 +28,76 @@
 </template>
 
 <script>
-  export default {
-    data(){
-      return {
-        courses: [{title:"A"},{title:"B"},{title:"C"}]
-      }
-    }
-  }
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      courses: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://demo-api-anima-project.herokuapp.com/courses")
+      .then((res) => {
+        res.data.forEach((course) => {
+          this.courses.push({
+            id: course.id,
+            title: course.title,
+          });
+        });
+      });
+  },
+};
 </script>
 
 <style>
-  table {
-    font-weight: 400;
-    border: var(--table-border) 1px solid;
-    border-spacing: 0;
-    background-color: var(--table-background);
-    font-size: 1.8rem;
+table {
+  font-weight: 400;
+  border-spacing: 0;
+  border: var(--table-border) 1px solid;
+  background-color: var(--table-background);
+  font-size: 1.8rem;
+  width: fit-content;
+}
 
-  }
+.table-header {
+  font-size: 2.4rem;
+  background-color: var(--table-header);
+}
 
-  .table-header {
-    font-size: 2.4rem;
-    background-color: var(--table-header);
-  }
+.course td {
+  border-top: var(--table-border) 1px solid;
+}
 
-  .warning-btn,
-  .danger-btn {
-    width: 8.7rem;
-    height: 2.2rem;
-    font-weight: 400;
-    color: var(--overlay);
-    font-size: 1.5rem;
-  }
+.warning-btn,
+.danger-btn {
+  width: 8.7rem;
+  height: 2.2rem;
+  font-weight: 400;
+  color: var(--overlay);
+  font-size: 1.5rem;
+}
 
-  .warning-btn {
-    background-color: var(--warning);
-  }
+.warning-btn {
+  background-color: var(--warning);
+}
 
-  .danger-btn {
-    background-color: var(--danger);
-  }
+.danger-btn {
+  background-color: var(--danger);
+}
 
-  .class {
-    margin-top: 2.1rem;
-  }
+.actions{
+  display: flex;
+  justify-content: space-between;
+  width: 20rem;
+}
+
+.course td:nth-child(1){
+  width: 50rem;
+}
+
+td{
+  padding: 0.5rem;
+}
 </style>
