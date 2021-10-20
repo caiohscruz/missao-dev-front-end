8736775982c8daf8a0ef30335d78e82c989b4cdb
caiohscruz <caiohscruz@hotmail.com>
2021-10-20 00:11:38 -0300

    add adm panel

--- src/views/AdministrationPanel.vue
+++ src/views/AdministrationPanel.vue
@@ -1,13 +1,73 @@
 <template>
-  
+  <div>
+    <h1>Painel Administrativo</h1>
+    <h4>Educação financeira é tudo de bom</h4>
+
+    <table>
+      <tr>
+        <td class="table-header">
+          Curso
+        </td>
+        <td class="table-header">
+          Ações
+        </td>
+      </tr>
+      <tr v-for="(course, index) in courses" :key="index">
+        <td>
+          {{course.title}}
+        </td>
+        <td>
+          <button class="warning-btn">editar</button>
+          <button class="danger-btn">excluir</button>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data(){
+      return {
+        courses: [{title:"A"},{title:"B"},{title:"C"}]
+      }
+    }
+  }
 </script>
 
 <style>
+  table {
+    font-weight: 400;
+    border: var(--table-border) 1px solid;
+    border-spacing: 0;
+    background-color: var(--table-background);
+    font-size: 1.8rem;
 
+  }
+
+  .table-header {
+    font-size: 2.4rem;
+    background-color: var(--table-header);
+  }
+
+  .warning-btn,
+  .danger-btn {
+    width: 8.7rem;
+    height: 2.2rem;
+    font-weight: 400;
+    color: var(--overlay);
+    font-size: 1.5rem;
+  }
+
+  .warning-btn {
+    background-color: var(--warning);
+  }
+
+  .danger-btn {
+    background-color: var(--danger);
+  }
+
+  .class {
+    margin-top: 2.1rem;
+  }
 </style>
